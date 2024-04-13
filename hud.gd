34bfd0b890f2015c41b3e13336370dde542d9fee
antoni <jedth@veril.mozmail.com>
2024-04-13 19:39:31 +0200

    add functionality to save highscores

--- hud.gd
+++ hud.gd
@@ -17,3 +17,26 @@ func _ready():
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta):
 	pass
+	
+
+const SAVE_PATH = "user://highscores.save"
+
+
+func insert_new_score(score):
+	var highscores := load_highscores()
+	highscores[Time.get_datetime_string_from_system()] = score
+	save_highscores(highscores)
+
+
+func load_highscores() -> Dictionary:
+	var file := FileAccess.open(SAVE_PATH, FileAccess.READ)
+	var json := JSON.new()
+	json.parse(file.get_line())
+	file.close()
+	return json.get_data() as Dictionary
+
+
+func save_highscores(highscores: Dictionary):
+	var file = FileAccess.open(SAVE_PATH, FileAccess.WRITE)
+	file.store_string(JSON.stringify(highscores))
+
