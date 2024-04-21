a3ea37b901909ba7d45f2a2353b59788890e9c7e
infwps562 <benjamin.schroeder@haw-hamburg.de>
2024-04-22 01:07:56 +0200

    improve game over menu further

--- hud.gd
+++ hud.gd
@@ -11,7 +11,6 @@ func _ready():
 		func ():
 			current_score += 1	
 			$ScoreCounter.text = str(current_score)
-			print("current score: ", current_score)
 	)
 
 
@@ -29,21 +28,22 @@ func show_game_over_menu():
 
 
 func create_highscore_table(highscores):
-	var score_table = "[center][table=2]"
+	var score_table = "[center][table=3]"
 	var pairs = []
 	for key in highscores:
 		pairs.append([key, highscores[key]])
 	pairs.sort_custom(func (a, b): return a[1] > b[1])
 	for pair in pairs.slice(0, 10):
 		var time = Time.get_datetime_dict_from_datetime_string(pair[0], false)
-		var time_string = " {0}/{1}/{2}".format([time.day, time.month, str(time.year).substr(2, 4)])
+		var time_string = " {0}/{1}/{2} {3}:{4}"\
+			.format([time.day, time.month, str(time.year).substr(2, 4), time.hour, time.minute])
 		var score_string = pair[1]
 		# turn current score green
 		if pair[0] == current_time:
 			var make_green = func(val): return "[color=green]{0}[/color]".format([val])
 			time_string = make_green.call(time_string)
 			score_string = make_green.call(score_string)
-		score_table += "[cell]{0}[/cell]\t[cell]{1}[/cell]".format([time_string, score_string])
+		score_table += "[cell]{0}[/cell][cell]\t\t[/cell][cell]{1}[/cell]".format([time_string, score_string])
 	score_table += "[/table][/center]"
 	return score_table
 
