896946ba5a89658838fd0f99d4ed074c34d5f8d2
infwps562 <benjamin.schroeder@haw-hamburg.de>
2024-04-22 01:34:30 +0200

    properly display times, refactor

--- hud.gd
+++ hud.gd
@@ -1,5 +1,7 @@
 extends CanvasLayer
 
+const SAVE_PATH = "user://highscores.save"
+
 var current_score: int
 var current_time: String
 
@@ -9,7 +11,7 @@ func _ready():
 	current_score = 0
 	SignalBus.player_passed_pipe.connect(
 		func ():
-			current_score += 1	
+			current_score += 1
 			$ScoreCounter.text = str(current_score)
 	)
 
@@ -34,9 +36,7 @@ func create_highscore_table(highscores):
 		pairs.append([key, highscores[key]])
 	pairs.sort_custom(func (a, b): return a[1] > b[1])
 	for pair in pairs.slice(0, 10):
-		var time = Time.get_datetime_dict_from_datetime_string(pair[0], false)
-		var time_string = " {0}/{1}/{2} {3}:{4}"\
-			.format([time.day, time.month, str(time.year).substr(2, 4), time.hour, time.minute])
+		var time_string = create_time_string(pair[0]) 
 		var score_string = pair[1]
 		# turn current score green
 		if pair[0] == current_time:
@@ -47,7 +47,16 @@ func create_highscore_table(highscores):
 	score_table += "[/table][/center]"
 	return score_table
 
-const SAVE_PATH = "user://highscores.save"
+
+func create_time_string(time: String):
+	var to_two_digits = func(s): return s if s.length() == 2 else "0" + s
+	var dict = Time.get_datetime_dict_from_datetime_string(time, false)
+	var day = to_two_digits.call(str(dict.day))
+	var month = to_two_digits.call(str(dict.month))
+	var year = str(dict.year).substr(2,4)
+	var hour = to_two_digits.call(str(dict.month))
+	var minute = to_two_digits.call(str(dict.month))
+	return "{0}/{1}/{2} {3}:{4}".format([day, month, year, hour, minute])
 
 
 func insert_new_score(score):
